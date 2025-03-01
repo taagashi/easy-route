4784a74246863d5bb074d683a2768ab0a479f642
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-03-01 16:20:57 -0300

    Registrada models no painel administrador do Django-Admin

--- easy_route/transport/admin.py
+++ easy_route/transport/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+from .models import Driver, Bus, Student, Institution, Route
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ['name', 'gmail', 'phone', 'photo']
+
+@admin.register((Student))
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'gmail', 'phone', 'photo', 'bus']
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'latitude', 'longitude', 'photo']
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ['going', 'back', 'institution']
+
+@admin.register(Bus)
+class BusAdmin(admin.ModelAdmin):
+    list_display = ['model', 'plate', 'capacityStudents', 'driver', 'route']
