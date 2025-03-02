bbdca0b2cc0229903e2cf314e396ffe693dad996
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-03-02 01:12:27 -0300

    Adicionado CRUD basico para realizar testes com as models

--- easy_route/transport/views.py
+++ easy_route/transport/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Driver, Bus, Student, Institution, Route
+from .serializers import DriverSerializer, BusSerializer, StudentSerializer, InstitutionSerializer, RouteSerializer
 
-# Create your views here.
+# realizando crud basico para testes
+
+class DriverViewSet(viewsets.ModelViewSet):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+class BusSerializer(viewsets.ModelViewSet):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class InstitutionViewSet(viewsets.ModelViewSet):
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
