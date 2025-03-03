067469eb8b9926a4537df34bd9abf0d79755370b
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-03-03 11:39:07 -0300

    Update urls

--- easy_route/transport/urls.py
+++ easy_route/transport/urls.py
@@ -4,23 +4,39 @@ from .views import BusRouteAPIView
 from django.urls import path, include
 
 urlpatterns = [
+    # cadastrar e listar alunos
     path('students/', StudentAPIView.as_view(), name='students'),
-    path('students/<int:pk>/', StudentListDeleteAPIView.as_view(), name='students_photo'),
-    path('students/<int:pk>/photo/', StudentAddPhotoAPIView.as_view(), name='student_list_delete'),
+    # listar aluno e deletar
+    path('students/<int:pk>/', StudentListDeleteAPIView.as_view(), name='students_list_delete'),
+    # adicionar foto para um aluno
+    path('students/<int:pk>/photo/', StudentAddPhotoAPIView.as_view(), name='student_photo'),
    
+    # adicionar e listar motoristas
     path('drivers/', DriverAPIView.as_view(), name='drivers'),
+    # listar e deletar motorista
     path('drivers/<int:pk>/', DriverListDeleteAPIView.as_view(), name='driver_list_delete'),
+    # adicionar foto para um motorista
     path('drivers/<int:pk>/photo/', DriverAddPhotoAPIView.as_view(), name='drivers_photo'),
     
+    # adicionar e listar instituicoes
     path('institutions/', InstitutionAPIView.as_view(), name='institutions'),
+    # listar e deletar instituicoes
     path('institutions/<int:pk>/', InstitutionListDeleteAPIView.as_view(), name='institutions_list_delete'),
+    # atualizar foto de instituicao
     path('institutions/<int:pk>/photo/', InstitutionViewPhotoAPIView.as_view(), name='institutions_photo'),
 
+    # adicionar e listar onibus
     path('bus/', BusAPIView.as_view(), name='bus'),
+    # listar e deletar onibus
     path('bus/<int:pk>/', BusListDeleteAPIView.as_view(), name='bus_list_delete'),
+    # atribuir rota para um onibus
+    path('bus/<int:pk>/routes', BusRouteAPIView.as_view(), name='bus_route'),
 
+    # adicionar e listar rotas
     path('routes/', RouteAPIView.as_view(), name='routes'),
+    # listar e deletar rotas
     path('routes/<int:pk>/', RouteListDeleteAPIView.as_view(), name='routes_list_delete'),
+    # adicionar instituicao para rota
     path('routes/<int:pk>/institutions/', RouteInstitutionsAPIView.as_view(), name='routes_institutions'),
-    path('routes/bus/<int:pk>/', BusRouteAPIView.as_view(), name='bus_route'),
+    
 ]
