029c423851aac4a7c3da949e2b8e3a489ce0521e
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-03-06 05:37:36 -0300

    adicionando novas migrations

--- easy_route/easy_route/settings.py
+++ easy_route/easy_route/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vvh(l61s4rxj9517)b2_7gzb#=9=ev5369yt(zx_er5o1mx(7l'
+SECRET_KEY = 'django-insecure-vvh(l61qs4rxj9517)b2_7gzb#=9=ev5369yt(zx_er5o1mx(7l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
